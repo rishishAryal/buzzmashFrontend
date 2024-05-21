516c234b35b518149075855774ec3d7391a193e5
Rishish Aryal <risharyal5@gmail.com>
2024-05-21 19:57:43 +0545

    dsdsd

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -36,7 +36,7 @@ export default function Login() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          email: formData.email,
+          email: formData.email, 
 
           password: formData.password,
         }),
